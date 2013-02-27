d2db9228ce7b347fc89cca0ee173c3683e1895ca
cnjinhao <cnjinhao@hotmail.com>
2013-02-28 04:09:05 +0800

    feature: - [in-develop] class place is updated

--- Nana.Cpp11/include/nana/gui/place.hpp
+++ Nana.Cpp11/include/nana/gui/place.hpp
@@ -13,6 +13,7 @@ namespace gui
 	{
 		typedef std::pair<window, unsigned>	fixed_t;
 		typedef std::pair<window, int>	percent_t;
+		typedef std::pair<window, std::pair<unsigned, unsigned> > room_t;
 
 		class number_t
 		{
@@ -34,7 +35,7 @@ namespace gui
 			{
 				if(kind::integer == kind_)
 					return value_.integer;
-				return value_.real;
+				return static_cast<int>(value_.real);
 			}
 
 			double real() const
@@ -77,7 +78,7 @@ namespace gui
 			{
 				enum class kind
 				{
-					window, gap, fixed, percent
+					window, gap, fixed, percent, room
 				};
 
 				kind kind_of_element;
@@ -87,6 +88,7 @@ namespace gui
 					unsigned gap_value;
 					fixed_t *	fixed_ptr;
 					percent_t * percent_ptr;
+					room_t *	room_ptr;
 				}u;
 
 				element_t(window wd)
@@ -113,8 +115,14 @@ namespace gui
 					u.percent_ptr = new percent_t(per);
 				}
 
+				element_t(const room_t& rm)
+					: kind_of_element(kind::room)
+				{
+					u.room_ptr = new room_t(rm);
+				}
+
 				element_t(const element_t& rhs)
-					: kind_of_element(rhs.kind_of_element), u(rhs.u)
+					: kind_of_element(rhs.kind_of_element)
 				{
 					switch(kind_of_element)
 					{
@@ -124,7 +132,12 @@ namespace gui
 					case kind::percent:
 						u.percent_ptr = new percent_t(*rhs.u.percent_ptr);
 						break;
-					default:	break;
+					case kind::room:
+						u.room_ptr = new room_t(*rhs.u.room_ptr);
+						break;
+					default:
+						u = rhs.u;
+						break;
 					}
 				}
 
@@ -139,6 +152,8 @@ namespace gui
 					case kind::percent:
 						rv.u.percent_ptr = nullptr;
 						break;
+					case kind::room:
+						rv.u.room_ptr = nullptr;
 					default:	break;
 					}
 				}
@@ -153,11 +168,16 @@ namespace gui
 					case kind::percent:
 						delete u.percent_ptr;
 						break;
+					case kind::room:
+						delete u.room_ptr;
+						break;
 					default:	break;
 					}
 				}
 			};
 		public:
+			typedef std::vector<element_t>::const_iterator const_iterator;
+
 			field_t()
 				: attached(false)
 			{}
@@ -183,6 +203,17 @@ namespace gui
 			field_t& operator<<(const percent_t& p)
 			{
 				seq_.emplace_back(p);
+				return *this;
+			}
+
+			field_t& operator<<(const room_t& r)
+			{
+				room_t x = r;
+				if(x.second.first == 0)
+					x.second.first = 1;
+				if(x.second.second == 0)
+					x.second.second = 1;
+				seq_.emplace_back(x);
 				return *this;
 			}
 
@@ -326,7 +357,7 @@ namespace gui
 								adj_px = adjustable_pixels;
 						}
 						else
-							adj_px = area.width * child->weight.real() / 100;
+							adj_px = static_cast<unsigned>(area.width * child->weight.real() / 100);
 					}
 					left += static_cast<int>(adj_px);
 					child->area.width = adj_px;
@@ -356,6 +387,10 @@ namespace gui
 							break;
 						case field_t::element_t::kind::window:
 							API::move_window(el.u.handle, left, r.y, adjustable_pixels, r.height);
+							left += static_cast<int>(adjustable_pixels);
+							break;
+						case field_t::element_t::kind::room:
+							API::move_window(el.u.room_ptr->first, left, r.y, adjustable_pixels, r.height);
 							left += static_cast<int>(adjustable_pixels);
 							break;
 						}
@@ -397,7 +432,7 @@ namespace gui
 								adj_px = adjustable_pixels;
 						}
 						else
-							adj_px = area.height * child->weight.real() / 100;
+							adj_px = static_cast<unsigned>(area.height * child->weight.real() / 100);
 					}
 					top += static_cast<int>(adj_px);
 					child->area.height = adj_px;
@@ -429,6 +464,10 @@ namespace gui
 							API::move_window(el.u.handle, r.x, top, r.width, adjustable_pixels);
 							top += static_cast<int>(adjustable_pixels);
 							break;
+						case field_t::element_t::kind::room:
+							API::move_window(el.u.room_ptr->first, r.x, top, r.width, adjustable_pixels);
+							top += static_cast<int>(adjustable_pixels);
+							break;
 						}
 					}
 				}
@@ -441,10 +480,184 @@ namespace gui
 		public:
 			div_grid(std::string&& name)
 				: division(kind::grid, std::move(name))
-			{}
+			{
+				dimension.first = dimension.second = 0;
+			}
 
 			virtual void collocate()
-			{}
+			{
+				if(dimension.first <= 1 && dimension.second <= 1)
+				{
+					std::size_t n_of_wd = _m_number_of_window();
+
+					std::size_t edge;
+					switch(n_of_wd)
+					{
+					case 0:
+					case 1:
+						edge = 1;	break;
+					case 2: case 3: case 4:
+						edge = 2;	break;
+					default:
+						edge = static_cast<std::size_t>(std::sqrt(n_of_wd));
+						if((edge * edge) < n_of_wd) ++edge;
+					}
+
+					auto i = field->container().begin(), end = field->container().end();
+
+					bool exit_for = false;
+					int y = area.y;
+					unsigned block_w = area.width / edge;
+					unsigned block_h = area.height / ((n_of_wd / edge) + (n_of_wd % edge ? 1 : 0));
+
+					for(std::size_t u = 0; u < edge; ++u)
+					{
+						int x = area.x;
+						for(std::size_t v = 0; v < edge; ++v)
+						{
+							i = _m_search(i, end);
+							if(i == end)
+							{
+								exit_for = true;
+								break;
+							}
+
+							window wd = nullptr;
+							unsigned value = 0;
+
+							switch(i->kind_of_element)
+							{
+							case field_t::element_t::kind::fixed:
+								wd = i->u.fixed_ptr->first;
+								value = i->u.fixed_ptr->second;
+								break;
+							case field_t::element_t::kind::percent:
+								wd = i->u.percent_ptr->first;
+								value = i->u.percent_ptr->second * area.width / 100;
+								break;
+							case field_t::element_t::kind::window:
+								wd = i->u.handle;
+								value = block_w;
+								break;
+							}
+							++i;
+							
+							API::move_window(wd, x, y, (value > block_w ? block_w : value), block_h);
+							x += static_cast<int>(block_w);
+						}
+						if(exit_for) break;
+						y += static_cast<int>(block_h);
+					}
+				}
+				else
+				{
+					unsigned block_w = area.width / dimension.first;
+					unsigned block_h = area.height / dimension.second;
+
+					char *table = new char[dimension.first * dimension.second];
+					memset(table, 0, dimension.first * dimension.second);
+
+					auto i = field->container().begin(), end = field->container().end();
+					std::size_t lbp = 0;
+					bool exit_for = false;
+
+					for(std::size_t c = 0; c < dimension.second; ++c)
+					{
+						for(std::size_t l = 0; l < dimension.first; ++l)
+						{
+							if(table[l + lbp])
+								continue;
+							
+							i = _m_search(i, end);
+							if(i == end)
+							{
+								exit_for = true;
+								break;
+							}
+
+							std::pair<unsigned, unsigned> room(1, 1);
+							if(i->kind_of_element == field_t::element_t::kind::room)
+							{
+								room = i->u.room_ptr->second;
+								if(room.first > dimension.first - l)
+									room.first = dimension.first - l;
+								if(room.second > dimension.second - c)
+									room.second = dimension.second - c;
+							}
+
+								window wd = nullptr;
+								unsigned value = 0;
+
+								switch(i->kind_of_element)
+								{
+								case field_t::element_t::kind::fixed:
+									wd = i->u.fixed_ptr->first;
+									value = i->u.fixed_ptr->second;
+									break;
+								case field_t::element_t::kind::percent:
+									wd = i->u.percent_ptr->first;
+									value = i->u.percent_ptr->second * area.width / 100;
+									break;
+								case field_t::element_t::kind::window:
+									wd = i->u.handle;
+									value = block_w;
+									break;
+								}
+
+								if(room.first <= 1 && room.second <= 1)
+								{
+									API::move_window(wd, area.x + l * block_w, area.y + c * block_h, block_w, block_h);
+									table[l + lbp] = 1;
+								}
+								else
+								{
+									API::move_window(i->u.room_ptr->first, area.x + l * block_w, area.y + c * block_h, block_w * room.first, block_h * room.second);
+
+									for(std::size_t y = 0; y < room.second; ++y)
+										for(std::size_t x = 0; x < room.first; ++x)
+										{
+											table[l + x + lbp + y * dimension.first] = 1;
+										}
+								}
+								++i;
+						}
+
+						if(exit_for)
+							break;
+
+						lbp += dimension.first;
+					}
+
+					delete [] table;
+				}
+			}
+
+		private:
+			static field_t::const_iterator _m_search(field_t::const_iterator i, field_t::const_iterator end)
+			{
+				if(i == end) return end;
+				
+				while(i->kind_of_element == field_t::element_t::kind::gap)
+				{
+					if(++i == end) return end;
+				}
+				return i;
+			}
+
+			std::size_t _m_number_of_window() const
+			{
+				if(nullptr == field) return 0;
+
+				std::size_t n = 0;
+				for(auto & el : field->container())
+				{
+					if(field_t::element_t::kind::gap != el.kind_of_element)
+						++n;
+				}
+				return n;
+			}
+		public:
+			std::pair<unsigned, unsigned> dimension;
 		};
 
 		struct implement
@@ -475,7 +688,7 @@ namespace gui
 			enum class token
 			{
 				div_start, div_end,
-				identifier, vertical, grid, number,
+				identifier, vertical, grid, number, array,
 				weight,
 				equal,
 				eof, error
@@ -495,9 +708,14 @@ namespace gui
 				return number_;
 			}
 
+			std::vector<number_t>& array()
+			{
+				return array_;
+			}
+
 			token read()
 			{
-				_m_eat_whitespace();
+				sp_ = _m_eat_whitespace(sp_);
 
 				std::size_t readbytes = 0;
 				switch(*sp_)
@@ -513,6 +731,33 @@ namespace gui
 				case '>':
 					++sp_;
 					return token::div_end;
+				case '[':
+					array_.clear();
+					sp_ = _m_eat_whitespace(sp_ + 1);
+					if(*sp_ == ']')
+					{
+						++sp_;
+						return token::array;
+					}
+
+					while(true)
+					{
+						sp_ = _m_eat_whitespace(sp_);
+						if(token::number != read())
+							_m_throw_error("invalid array element");
+
+						array_.push_back(number_);
+
+						sp_ = _m_eat_whitespace(sp_);
+						char ch = *sp_++;
+						if(ch != ',')
+						{
+							if(ch == ']')
+								break;
+							_m_throw_error("invalid array");
+						}
+					}
+					return token::array;
 				case '.': case '-':
 					if(*sp_ == '-')
 					{
@@ -531,13 +776,17 @@ namespace gui
 					else
 						_m_throw_error(*sp_);
 					break;
-				}
-
-				readbytes = _m_number(sp_, false);
-				if(readbytes)
-				{
-					sp_ += readbytes;
-					return token::number;
+				default:
+					if('0' <= *sp_ && *sp_ <= '9')
+					{
+						readbytes = _m_number(sp_, false);
+						if(readbytes)
+						{
+							sp_ += readbytes;
+							return token::number;
+						}
+					}
+					break;
 				}
 
 				char ch = *sp_;
@@ -598,24 +847,25 @@ namespace gui
 				throw std::runtime_error(ss.str());
 			}
 
-			void _m_eat_whitespace()
+			const char* _m_eat_whitespace(const char* sp)
 			{
-				while(*sp_)
+				while(*sp)
 				{
-					switch(*sp_)
+					switch(*sp)
 					{
 					case ' ': case '\t': case '\r': case '\n':
-						++sp_;	break;
+						++sp;	break;
 					default:
-						return;
+						return sp;
 					}
-				}			
+				}
+				return sp;
 			}
 
 			std::size_t _m_number(const char* sp, bool negative)
 			{
 				const char* allstart = sp;
-				_m_eat_whitespace();
+				sp = _m_eat_whitespace(sp);
 
 				number_.assign(0);
 
@@ -631,10 +881,10 @@ namespace gui
 				}
 				const char* iend = sp;
 
-				if('.' == *sp++)
+				if('.' == *sp)
 				{
 					double div = 1;
-					const char* rstart = sp;
+					const char* rstart = ++sp;
 					while('0' <= *sp && *sp <= '9')
 					{
 						real += (*sp - '0') / (div *= 10);
@@ -661,9 +911,9 @@ namespace gui
 					{
 						if(number_t::kind::integer == number_.kind_of())
 							number_.assign_percent(number_.integer());
-						return sp - allstart;
+						return sp - allstart + 1;
 					}
-					return sp - allstart - 1;
+					return sp - allstart;
 				}
 				return 0;
 			}
@@ -672,6 +922,7 @@ namespace gui
 			const char* sp_;
 			std::string idstr_;
 			number_t number_;
+			std::vector<number_t> array_;
 		};
 	public:
 		place()
@@ -709,6 +960,11 @@ namespace gui
 		static percent_t percent(window wd, int per)
 		{
 			return percent_t(wd, per);
+		}
+
+		static room_t room(window wd, unsigned w, unsigned h)
+		{
+			return room_t(wd, std::pair<unsigned, unsigned>(w, h));
 		}
 
 		field_t& field(const char* name)
@@ -778,6 +1034,7 @@ namespace gui
 			std::string name;
 
 			number_t weight;
+			std::vector<number_t> array;
 
 			std::vector<division*> children;
 			for(token tk = tknizer.read(); tk != token::eof; tk = tknizer.read())
@@ -792,8 +1049,15 @@ namespace gui
 				case token::grid:
 					div_type = tk;
 					break;
+				case token::array:
+					tknizer.array().swap(array);
+					break;
 				case token::weight:
 					weight = tknizer.number();
+					//If the weight is type of real, convert it to integer.
+					//the integer and percent are allowed for weight.
+					if(weight.kind_of() == number_t::kind::real)
+						weight.assign(static_cast<int>(weight.real()));
 					break;
 				case token::div_end:
 					exit_for = true;
@@ -829,10 +1093,32 @@ namespace gui
 				div = new div_vertical_arrange(std::move(name));
 				break;
 			case token::grid:
-				div = new div_grid(std::move(name));
+				{
+					div_grid * p = new div_grid(std::move(name));
+					
+					if(array.size())
+					{
+						if(array[0].kind_of() != number_t::kind::percent)
+							p->dimension.first = array[0].integer();
+					}
+					
+					if(array.size() > 1)
+					{
+						if(array[1].kind_of() != number_t::kind::percent)
+							p->dimension.second = array[1].integer();
+					}
+
+					if(0 == p->dimension.first)
+						p->dimension.first = 1;
+
+					if(0 == p->dimension.second)
+						p->dimension.second = 1;
+
+					div = p;
+				}
 				break;
 			}
-
+			
 			div->weight = weight;
 			div->field = field;
 			div->children.swap(children);
